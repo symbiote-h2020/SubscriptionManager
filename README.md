c2641c7b0e182b8148db82cf6848fb721de31605
vthglyk <vasgl@intracom-telecom.com>
2018-01-29 17:25:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+[![Build Status](https://api.travis-ci.org/symbiote-h2020/SubscriptionManager.svg?branch=staging)](https://api.travis-ci.org/symbiote-h2020/SubscriptionManager)
+[![codecov.io](https://codecov.io/github/symbiote-h2020/SubscriptionManager/branch/master/graph/badge.svg)](https://codecov.io/github/symbiote-h2020/SubscriptionManager)
+
+# Subscription Manager
---
+++ src/main/java/eu/h2020/symbiote/subman/SubscriptionManager.java
@@ -0,0 +1,16 @@
+package eu.h2020.symbiote.subman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient    
+@EnableAutoConfiguration
+@SpringBootApplication
+public class SubscriptionManager {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionManager.class, args);
+    }
+}
