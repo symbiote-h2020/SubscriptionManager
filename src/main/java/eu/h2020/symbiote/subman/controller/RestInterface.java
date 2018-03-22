e0892b604087545490f2fcbe16834a295a728484
pKrivic23 <petar.krivic@fer.hr>
2018-03-22 16:16:49 +0100

    -added mapping static methods

--- src/main/java/eu/h2020/symbiote/subman/controller/RestInterface.java
+++ src/main/java/eu/h2020/symbiote/subman/controller/RestInterface.java
@@ -1,7 +1,10 @@
 package eu.h2020.symbiote.subman.controller;
 
+import java.io.IOException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -12,6 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import eu.h2020.symbiote.cloud.model.internal.ResourcesAddedOrUpdatedMessage;
+import eu.h2020.symbiote.cloud.model.internal.ResourcesDeletedMessage;
 import eu.h2020.symbiote.subman.messaging.RabbitManager;
 
 /**
@@ -27,6 +34,8 @@ public class RestInterface {
 	private RabbitManager rabbitManager;
 
 	private SecurityManager securityManager;
+	
+	public static ObjectMapper om = new ObjectMapper();
 	
 	@Autowired
     public RestInterface(RabbitManager rabbitManager, SecurityManager securityManager) {
@@ -45,6 +54,7 @@ public class RestInterface {
 			@RequestBody String receivedJson) {
 
 		logger.info("resourcesAddedOrUpdated HTTP-POST request received.");
+		
 		//TODO check if received platformId belongs to received federationId
 		
 		String senderPlatformId = null; //will receive in federatedResourceId
@@ -100,5 +110,29 @@ public class RestInterface {
 	public ResponseEntity<?> subscriptionRemoval(@RequestHeader HttpHeaders httpHeaders, @RequestBody String receivedJson) {
 		//TODO implement subscription model and receiving definition request
 		return new ResponseEntity<>(httpHeaders, HttpStatus.LOCKED);
+	}
+	
+	public static ResourcesAddedOrUpdatedMessage mapAddedOrUpdatedMessage(String json){
+		
+		ResourcesAddedOrUpdatedMessage received;
+		try {
+			received = om.readValue(json, ResourcesAddedOrUpdatedMessage.class);
+		} catch (IOException e) {
+			logger.info("Exception trying to map received ResourcesAddedOrUpdatedMessage json to object!");
+			return null;
+		}
+		return received;
+	}
+	
+	public static ResourcesDeletedMessage mapDeletedMessage(String json){
+		
+		ResourcesDeletedMessage received;
+		try {
+			received = om.readValue(json, ResourcesDeletedMessage.class);
+		} catch (IOException e) {
+			logger.info("Exception trying to map received ResourcesDeletedMessage json to object!");
+			return null;
+		}
+		return received;
 	}
 }
