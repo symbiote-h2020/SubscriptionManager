9132b0850149fe050efcbfeac31931632e21dd58
vthglyk <vasgl@intracom-telecom.com>
2018-05-02 14:14:16 +0300

    Maven publishing fat jar

--- src/main/java/eu/h2020/symbiote/subman/SubscriptionManager.java
+++ src/main/java/eu/h2020/symbiote/subman/SubscriptionManager.java
@@ -6,12 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
-//@EnableDiscoveryClient    
+@EnableDiscoveryClient
 @EnableAutoConfiguration
 @SpringBootApplication
 public class SubscriptionManager {
 
 	public static void main(String[] args) {
+		WaitForPort.waitForServices(WaitForPort.findProperty("SPRING_BOOT_WAIT_FOR_SERVICES"));
 		SpringApplication.run(SubscriptionManager.class, args);
     }
 }
