97ec1b94931726c526e0b8a942306576c06d0ee8
pKrivic23 <petar.krivic@fer.hr>
2018-05-17 15:08:37 +0200

    -test for own subscription init

--- src/test/java/eu/h2020/symbiote/subman/SubscriptionManagerTest.java
+++ src/test/java/eu/h2020/symbiote/subman/SubscriptionManagerTest.java
@@ -1,11 +1,17 @@
 package eu.h2020.symbiote.subman;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import eu.h2020.symbiote.subman.repositories.SubscriptionRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
@@ -13,8 +19,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 @ActiveProfiles("test")
 public class SubscriptionManagerTest {
 
+	@Autowired
+	SubscriptionRepository subscriptionRepo;
+	
+	@Value("${platform.id}")
+	private String platformId;
+	
 	@Test
 	public void contextLoads() {
+		assertEquals(1, subscriptionRepo.findAll().size());
 	}
 
 }
