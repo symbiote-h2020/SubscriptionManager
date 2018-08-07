5acea3dd611fd17ccae841827e1520406bf18477
vthglyk <vasgl@intracom-telecom.com>
2018-08-07 12:52:31 +0300

    Adding configurable mongo host

--- src/main/java/eu/h2020/symbiote/subman/AppConfig.java
+++ src/main/java/eu/h2020/symbiote/subman/AppConfig.java
@@ -3,8 +3,10 @@ package eu.h2020.symbiote.subman;
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import java.util.Collection;
@@ -13,6 +15,9 @@ import java.util.Collections;
 @Configuration
 @EnableMongoRepositories
 public class AppConfig extends AbstractMongoConfiguration {
+
+    @Value("${spring.data.mongodb.host:localhost}")
+    private String mongoHost;
 
     @Value("${symbiote.cloud.subscriptionManager.database}")
     private String databaseName;
@@ -29,5 +34,11 @@ public class AppConfig extends AbstractMongoConfiguration {
 
     @Override
     protected Collection<String> getMappingBasePackages() { return Collections.singletonList("com.oreilly.springdata.mongodb"); }
+
+    @Bean
+    @Override
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(new MongoClient(mongoHost), getDatabaseName());
+    }
 
 }
